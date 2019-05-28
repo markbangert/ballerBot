ac7590301f8e6d20e5735df80ca47baf15b110e5
Mark Bangert <mark.bangert@analytical.software.de>
2019-05-28 21:56:51 +0200

    initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=ballerbotweb-Web-Deploy.pubxml -p:Password=SDqlcv4SpgRvPqQzR2aKb6CooMw3oi310xqjTCo5t1Rdexss7XsuNSszLWPS
+
